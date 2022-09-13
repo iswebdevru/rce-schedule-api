08e040989c15d448259f691bf027b49ad6d32ed6
Alexander Ward <63157919+wardxela@users.noreply.github.com>
2022-09-13 21:26:27 +0300

    change: enable cors for all domains

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -2,5 +2,5 @@ import fp from 'fastify-plugin';
 import cors from '@fastify/cors';
 
 export default fp(async fastify => {
-  fastify.register(cors, { origin: 'http://127.0.0.1:5173' });
+  fastify.register(cors, { origin: '*' });
 });
